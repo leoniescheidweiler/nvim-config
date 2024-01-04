7406bb1e17210680b03dc72fba6a0a19713cf4cc
Leonie Scheidweiler <leonie.olivia.scheidweiler@gmail.com>
2024-01-04 17:06:13 +0100

    tab and space handling

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,10 @@
 local options = {
 	number = true,
-	scrolloff = 8
+	scrolloff = 8,
+
+	tabstop = 8, -- display tab character as 8 spaces
+	expandtab = true, -- expand tab character into spaces
+	shiftwidth = 4, -- use 4 space indents
 }
 
 for k, v in pairs(options) do
